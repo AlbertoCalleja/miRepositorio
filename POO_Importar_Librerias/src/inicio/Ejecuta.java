770630160a6c45d99ae0aec002b2ec1ace4cdc44
usr <acp22598@gmail.com>
2021-09-23 13:55:28 +0200

    Alberto 23-09-2021 POO_Importar_Libreria- ejemplo

---
+++ POO_Importar_Librerias/src/inicio/Ejecuta.java
@@ -0,0 +1,13 @@
+package inicio;
+
+import paquete.Vacaciones; //Incluir la clase
+
+public class Ejecuta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vacaciones v= new Vacaciones(14,"Colombia",true);
+		v.mostrar();
+	}
+
+}
